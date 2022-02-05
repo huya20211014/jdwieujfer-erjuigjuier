41d2425c948c8eaf563922cd218e1f44763af53d
muyangren907 <admin@chaoshang.com>
2022-02-05 10:04:51 +0000

    add herokuArgsSanic.py

--- herokusanic.py
+++ herokusanic.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+# @Time    : 2022/2/5 17:44
+# @Author  : muyangren907
+# @Email   : myr907097904@gmail.com
+# @File    : herokuArgsSanic.py
+# @Software: PyCharm
+# -*- coding: utf-8 -*-
 # @Time    : 2021/10/10 20:47
 # @Author  : muyangren907
 # @Email   : myr907097904@gmail.com
@@ -55,35 +61,53 @@ async def index(request):
     huyadis = 'Hello World'
     return sanic.response.text(str(huyadis))
 
-
-@app.route('/record')
+@app.route('/api')
 async def index(request):
-    huyadis = [mp4_ for mp4_ in os.listdir('record') if '.mp4' in mp4_]
-    huyalen = len(huyadis)
-    huyadis.sort()
-    responsestr = '<h>正在录制 {} 个</h></br>'.format(huyalen)
-    for mp4_ in huyadis:
-        responsestr+='<a>{} : {}</a></br>'.format(mp4_,getsizestr(os.path.join('record',mp4_)))
-    return sanic.response.html(responsestr)
+    qury_type = request.args['qury_type']
+    qury_type_val = os.environ.get("{}".format(qury_type))
+    return sanic.response.text(qury_type_val)
 
-@app.route('/recordok')
-async def index(request):
-    huyadis = [mp4_ for mp4_ in os.listdir('recordok') if '.mp4' in mp4_]
-    huyalen = len(huyadis)
-    huyadis.sort()
-    responsestr = '<h>录制完成 {} 个</h></br>'.format(huyalen)
-    for mp4_ in huyadis:
-        responsestr+='<a>{} : {}</a></br>'.format(mp4_,getsizestr(os.path.join('recordok',mp4_)))
-    return sanic.response.html(responsestr)
+
+
+# @app.route('/recordok')
+# async def index(request):
+#     huyadis = [mp4_ for mp4_ in os.listdir('recordok') if '.mp4' in mp4_]
+#     huyalen = len(huyadis)
+#     huyadis.sort()
+#     responsestr = '<h>录制完成 {} 个</h></br>'.format(huyalen)
+#     for mp4_ in huyadis:
+#         responsestr+='<a>{} : {}</a></br>'.format(mp4_,getsizestr(os.path.join('recordok',mp4_)))
+#     return sanic.response.html(responsestr)
+
+
+# @app.route('/record')
+# async def index(request):
+#     huyadis = [mp4_ for mp4_ in os.listdir('record') if '.mp4' in mp4_]
+#     huyalen = len(huyadis)
+#     huyadis.sort()
+#     responsestr = '<h>正在录制 {} 个</h></br>'.format(huyalen)
+#     for mp4_ in huyadis:
+#         responsestr+='<a>{} : {}</a></br>'.format(mp4_,getsizestr(os.path.join('record',mp4_)))
+#     return sanic.response.html(responsestr)
+#
+# @app.route('/recordok')
+# async def index(request):
+#     huyadis = [mp4_ for mp4_ in os.listdir('recordok') if '.mp4' in mp4_]
+#     huyalen = len(huyadis)
+#     huyadis.sort()
+#     responsestr = '<h>录制完成 {} 个</h></br>'.format(huyalen)
+#     for mp4_ in huyadis:
+#         responsestr+='<a>{} : {}</a></br>'.format(mp4_,getsizestr(os.path.join('recordok',mp4_)))
+#     return sanic.response.html(responsestr)
 
 if __name__ == '__main__':
     USE_PROXY = True
     # LISTEN_PORT = 8888
     # listen_port = LISTEN_PORT
-    huyathth = huyath()
-    huyathth.start()
-    upthth = upth()
-    upthth.start()
+    # huyathth = huyath()
+    # huyathth.start()
+    # upthth = upth()
+    # upthth.start()
 
     app.run(host='0.0.0.0',
             port=int(os.environ.get('PORT', 8000)),
