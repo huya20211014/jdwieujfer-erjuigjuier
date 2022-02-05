acc45204bcef36180b9af4caab3c726ad8b15164
muyangren907 <admin@chaoshang.com>
2022-02-05 10:50:21 +0000

    add herokuArgsSanic.py

--- herokusanic.py
+++ herokusanic.py
@@ -131,5 +131,4 @@ if __name__ == '__main__':
 
     app.run(host='0.0.0.0',
             port=int(os.environ.get('PORT', 8000)),
-            workers=int(os.environ.get('WEB_CONCURRENCY', 1)),
             debug=bool(os.environ.get('DEBUG', '')))
