5d85302faf27cb166dbc0b363319f85d9e9ee05c
muyangren907 <907097904@qq.com>
2022-05-20 08:04:47 +0000

    add dymcmn18

--- docker/app.py
+++ docker/app.py
@@ -173,7 +173,7 @@ def get_uflag():
 
 if __name__ == '__main__':
     #ksmullive_idx = int(os.environ.get("ksmullive_idx"))
-    xxxxLOAD_PRE = 'Douyin_dymcmn19'
+    xxxxLOAD_PRE = 'Douyin_dymcmn18'
     post_global = ['.tt','.mp4', '.jpg', '.jpeg', '.png', '.mov', '.MP4', '.JPG', '.JPEG', '.PNG', '.gif', '.GIF']
     # post_global = []
     max_thread_num_MAX = 2
