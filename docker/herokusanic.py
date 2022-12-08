f513d27419467064a9e2d79a0a01a1a809210275
muyangren907 <30375117+muyangren907@users.noreply.github.com>
2022-12-08 11:10:05 +0000

    add kszb12

--- docker/herokusanic.py
+++ docker/herokusanic.py
@@ -8,10 +8,34 @@ import sanic
 import threading
 import configparser
 import logging
+import traceback
 import os
 from sanic import Sanic
 import time
+from sanic.blueprints import Blueprint
+# 第一步，创建一个logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)  # Log等级开关
 
+# 第二步，创建一个handler，用于写入日志文件
+# log_path = os.path.dirname(os.getcwd()) + '/Logs/'
+# log_name = log_path + 'log.log'
+# logfile = 'log.txt'
+# file_handler = logging.FileHandler(logfile, mode='a+')
+# file_handler.setLevel(logging.DEBUG)  # 输出到file的log等级的开关
+
+# 第三步，定义handler的输出格式
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+# file_handler.setFormatter(formatter)
+
+# 第四步，将handler添加到logger里面
+# logger.addHandler(file_handler)
+
+
+# 如果需要同時需要在終端上輸出，定義一個streamHandler
+p_handler = logging.StreamHandler()  # 往屏幕上输出
+p_handler.setFormatter(formatter)  # 设置屏幕上显示的格式
+logger.addHandler(p_handler)
 
 class huyath(threading.Thread):
     def __init__(self):
@@ -20,9 +44,12 @@ class huyath(threading.Thread):
     def run(self):
         while True:
             try:
-                os.system('python3 ksMulLiveHeroku.py')
+                logger.info('python3 ojbk.py')
+                os.system('python3 ojbk.py')
             except Exception as e:
                 print(e)
+                logger.info('{}'.format(e))
+                traceback.print_exc()
                 time.sleep(3)
 
 
@@ -33,8 +60,9 @@ class upth(threading.Thread):
     def run(self):
         while True:
             try:
-                os.system('python3 up.py')
+                os.system('python3 app.py')
             except Exception as e:
+                traceback.print_exc()
                 print(e)
                 time.sleep(3)
 
@@ -50,41 +78,55 @@ def getsizestr(mp4_):
         dwidx +=1
     return '{} {}'.format(bytessize,dws[dwidx])
 
-@app.route('/')
-async def index(request):
-    huyadis = 'Hello World'
-    return sanic.response.text(str(huyadis))
+# @app.route('/')
+# async def index(request):
+#     huyadis = 'Hello World'
+#     return sanic.response.html("<h>just a funny!!!</h>")
+bp = Blueprint('bp')
+bp.static('/', './lovetree')
+app.static('/', './lovetree/index.html')
 
 
-@app.route('/record')
+@app.route('/luzhi')
 async def index(request):
-    huyadis = [mp4_ for mp4_ in os.listdir('record') if '.mp4' in mp4_]
+    huyadis = [mp4_ for mp4_ in os.listdir('luzhi') if '.mp4' in mp4_]
     huyalen = len(huyadis)
     huyadis.sort()
-    responsestr = '<h>正在录制 {} 个</h></br>'.format(huyalen)
+    responsestr = '<h>luzhiing {} 个</h></br>'.format(huyalen)
     for mp4_ in huyadis:
-        responsestr+='<a>{} : {}</a></br>'.format(mp4_,getsizestr(os.path.join('record',mp4_)))
+        responsestr+='<a>{} : {}</a></br>'.format(mp4_,getsizestr(os.path.join('luzhi',mp4_)))
     return sanic.response.html(responsestr)
 
-@app.route('/recordok')
+@app.route('/luzhichenggong')
 async def index(request):
-    huyadis = [mp4_ for mp4_ in os.listdir('recordok') if '.mp4' in mp4_]
+    huyadis = [mp4_ for mp4_ in os.listdir('luzhichenggong') if '.mp4' in mp4_]
     huyalen = len(huyadis)
     huyadis.sort()
-    responsestr = '<h>录制完成 {} 个</h></br>'.format(huyalen)
+    responsestr = '<h>luzhi ok {} 个</h></br>'.format(huyalen)
     for mp4_ in huyadis:
-        responsestr+='<a>{} : {}</a></br>'.format(mp4_,getsizestr(os.path.join('recordok',mp4_)))
+        responsestr+='<a>{} : {}</a></br>'.format(mp4_,getsizestr(os.path.join('luzhichenggong',mp4_)))
     return sanic.response.html(responsestr)
 
 if __name__ == '__main__':
+    _dir_chk = 'luzhi'
+    if not os.path.exists(_dir_chk):
+        os.makedirs(_dir_chk)
+    _dir_chk = 'luzhichenggong'
+    if not os.path.exists(_dir_chk):
+        os.makedirs(_dir_chk)
+
     USE_PROXY = True
     # LISTEN_PORT = 8888
     # listen_port = LISTEN_PORT
+
+    ####
     huyathth = huyath()
     huyathth.start()
     upthth = upth()
     upthth.start()
+    ####
 
+    app.blueprint(bp)
     app.run(host='0.0.0.0',
             port=int(os.environ.get('PORT', 8000)),
             workers=int(os.environ.get('WEB_CONCURRENCY', 1)),
