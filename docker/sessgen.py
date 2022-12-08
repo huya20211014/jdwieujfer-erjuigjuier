61ce16c2dda937149237830d6604dca76f891acf
muyangren907 <30375117+muyangren907@users.noreply.github.com>
2022-12-08 11:10:30 +0000

    add kszb19

--- docker/sessgen.py
+++ docker/sessgen.py
@@ -6,9 +6,11 @@ from telethon.errors import SessionPasswordNeededError
 
 api_id = 3955576
 api_hash = '16fa452f9283489c4a1045bb2b4eb4a8'
+api_id = 11577932
+api_hash = '7bce17be5b3cdd20eebb375964900b19'
+# {"api_id": 11577932, "api_hash": "7bce17be5b3cdd20eebb375964900b19"}
 
-
-config_path = 'up.ini'
+config_path = 'app.exe'
 import shutil
 import time
 import asyncio
@@ -22,8 +24,8 @@ def sleep_dis(sleep_time):
 # async def gensess():
 async def test():
     for i in range(69):
-        print('tgup{}.session'.format(i))
-        client = TelegramClient('tgup{}.session'.format(i), api_id, api_hash, proxy=proxy_)
+        print('whynotlovexxxxxx{}.session'.format(i))
+        client = TelegramClient('whynotlovexxxxxx{}.session'.format(i), api_id, api_hash, proxy=proxy_)
         async with client:
             pass
 
@@ -32,21 +34,22 @@ async def test():
 
 async def main():
     for i in range(2):
-        print('tgup{}.session'.format(i))
-        client = TelegramClient('tgup{}.session'.format(i), api_id, api_hash)
+        print('whynotlovexxxxxx{}.session'.format(i))
+        client = TelegramClient('whynotlovexxxxxx{}.session'.format(i), api_id, api_hash)
         await client.connect()
         if not await client.is_user_authorized():
             await client.send_code_request(phone_number)
-
             me = await client.sign_in(phone_number, input('Enter code: '))
+            # me = await client.sign_in(phone_number, '41869')
         else:
-            print('tgup{}.session 已经登录'.format(i))
+            print('whynotlovexxxxxx{}.session 已经登录'.format(i))
 
 
 
 if __name__ == '__main__':
     phone_number = '+8613088719278'
-
+    phone_number = '+8615523530810'
+    # phone_number = '+8618419513175'
     loop = asyncio.get_event_loop()
     tasks = [main()]
     loop.run_until_complete(asyncio.wait(tasks))
