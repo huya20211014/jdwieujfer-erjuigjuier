571a11c8253635db1376f52b960b2c56e133a576
muyangren907 <30375117+muyangren907@users.noreply.github.com>
2023-02-14 16:00:04 +0000

    add kszb01

--- docker/1.py
+++ docker/1.py
@@ -2,9 +2,9 @@ for i in range(1,21):
     key1 = 'kszb%02d'%i
     keyy = '{}'.format(key1)
     cmd = 'git checkout {} && git checkout douyinzhibo01sby -- lovetree'.format(keyy)
-    cmd += '&& git checkout {} && git checkout kszb01 -- ksMulLiveHeroku.py'.format(keyy)
+    cmd += '&& git checkout kszb01 -- ksMulLiveHeroku.py'
     
-    cmd += "&& sed -i 's/ksmullive_idx=1/ksmullive_idx={}/g' douyinMulLiveAioHeroku.py".format(i)
+    cmd += "&& sed -i 's/ksmullive_idx=1/ksmullive_idx={}/g' ksMulLiveHeroku.py".format(i)
     cmd +='&& git add -A && git commit -m "add {}" && git push origin {}'.format(keyy,keyy)
     
     print(cmd)
