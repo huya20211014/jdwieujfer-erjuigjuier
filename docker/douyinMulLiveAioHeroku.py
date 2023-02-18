0a3676be99a3ee038d5c8c32c5002e12cdc88a2f
muyangren907 <30375117+muyangren907@users.noreply.github.com>
2023-02-18 09:30:39 +0000

    add douyinzhibo45xby

--- docker/douyinMulLiveAioHeroku.py
+++ docker/douyinMulLiveAioHeroku.py
@@ -678,6 +678,6 @@ if __name__ == '__main__':
     # luzhishichang = 1800
     # ids_file_path = 'URL_config.ini'
     # ids_file_path = 'ids.txt'
-    SLEEP_TIME = 60
+    SLEEP_TIME = 60*2
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
