108735ae457bd6e77399c08137f34d6db0147123
muyangren907 <30375117+muyangren907@users.noreply.github.com>
2023-02-19 10:41:35 +0000

    add dymcmn02xby

---
+++ docker/1.py
@@ -0,0 +1,9 @@
+for i in range(1,49):
+    key1 = 'douyinzhibo%02d'%i
+    keyy = '{}xby'.format(key1)
+    cmd = 'git checkout {} && git checkout douyinzhibo08xby -- douyinMulLiveAioHeroku.py'.format(keyy)
+    
+    cmd += "&& sed -i 's/douyinzhibo01_web/{}_web/g' douyinMulLiveAioHeroku.py".format(key1)
+    cmd +='&& git add -A && git commit -m "add {}" && git push origin {}'.format(keyy,keyy)
+    
+    print(cmd)
